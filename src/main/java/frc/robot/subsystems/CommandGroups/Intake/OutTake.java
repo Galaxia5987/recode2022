e2548059b0b9971a8f05e85e8b8f1c18bbbadabc
Alon <alon.schwierz@gmail.com>
2022-09-13 18:44:23 +0300

    add Intake CommandGroup

--- src/main/java/frc/robot/subsystems/CommandGroups/Intake/OutTake.java
+++ src/main/java/frc/robot/subsystems/CommandGroups/Intake/OutTake.java
@@ -2,16 +2,15 @@ package frc.robot.subsystems.CommandGroups.Intake;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.intake.Intake;
 
-public class feedAndConvey extends ParallelCommandGroup {
-    public feedAndConvey(Intake intake, Conveyor conveyor){
+public class OutTake extends ParallelCommandGroup {
+    public OutTake(Intake intake, Conveyor conveyor) {
         addCommands(
-                new RunCommand(()->intake.setPower(0.5)),
-                new RunCommand(()->conveyor.feedFromIntake(0.5))
+                new RunCommand(() -> intake.setPower(-0.5)),
+                new RunCommand(() -> conveyor.feedFromIntake(-0.5))
         );
     }
 }
