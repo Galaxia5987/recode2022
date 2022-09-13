dcfe12664b8daed2e9b87b4458b83cef352d0b89
eitan <eitan.vichik@gmail.com>
2022-09-13 18:26:42 +0300

    Add intake toggle command

---
+++ src/main/java/frc/robot/subsystems/intake/commands/ToggleIntake.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.intake.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.intake.Intake;
+
+public class ToggleIntake extends InstantCommand {
+
+    public ToggleIntake() {
+        super(() -> Intake.getInstance().toggleMechanism());
+    }
+}
