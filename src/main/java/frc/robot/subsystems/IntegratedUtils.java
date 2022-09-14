461595b9c516f8ad7c6578588568f140f6ae4761
eitan <eitan.vichik@gmail.com>
2022-09-14 12:29:26 +0300

    Add integrated utils explanation

--- src/main/java/frc/robot/subsystems/IntegratedUtils.java
+++ src/main/java/frc/robot/subsystems/IntegratedUtils.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems;
 
 import frc.robot.Constants;
-import frc.robot.Robot;
 import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
 import frc.robot.subsystems.helicopter.Helicopter;
@@ -10,6 +9,12 @@ import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.vision.Limelight;
 
+/**
+ * This class constitutes all the information provided by multiple subsystem.
+ *
+ * Here one can implement useful functions without implementing suppliers
+ * in the code, and ensure the same information for all commands.
+ */
 public class IntegratedUtils {
     private static final Shooter shooter = Shooter.getInstance();
     private static final Limelight limelight = Limelight.getInstance();
@@ -19,10 +24,16 @@ public class IntegratedUtils {
     private static final Hood hood = Hood.getInstance();
     private static final Helicopter helicopter = Helicopter.getInstance();
 
+    /**
+     * This function gets the distance to the target in two ways:
+     *      vision - if the vision has targets
+     *      odometry - if the vision doesn't have targets
+     * @return the distance to the target. [m]
+     */
     public static double distanceToTarget() {
-        return limelight.estimatePose(Robot.getAngle())
-                .orElse(swerveDrive.getPose())
+        return limelight.getDistance()
+                .orElse(swerveDrive.getPose()
                 .minus(Constants.Vision.HUB_POSE)
-                .getTranslation().getNorm();
+                .getTranslation().getNorm());
     }
 }
